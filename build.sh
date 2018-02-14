e2fe87a97fc8d1022d42cfae2f4ebb81d8e5a525
DDoSolitary <DDoSolitary@gmail.com>
2018-02-14 16:40:35 +0800

    Add simple-obfs.

--- build.sh
+++ build.sh
@@ -13,28 +13,36 @@ git config --global user.name DDoSolitary
 git config --global user.email DDoSolitary@gmail.com
 git config --global core.autocrlf false
 git clone https://github.com/shadowsocks/shadowsocks-libev
+git clone https://github.com/shadowsocks/simple-obfs
 git clone git@github.com:DDoSolitary/shadowsocks-libev-win -b $release_branch
 
 mkdir -p stat
-touch stat/installed.db stat/commit
-git_commit="$(git --git-dir=shadowsocks-libev/.git show -s --format=%H)"
-if diff /etc/setup/installed.db stat/installed.db && [ "$(cat stat/commit)" == "$git_commit" ]; then
+touch stat/installed.db stat/ss-commit stat/obfs-commit
+ss_commit="$(git --git-dir=shadowsocks-libev/.git show -s --format=%H)"
+obfs_commit="$(git --git-dir=simple-obfs/.git show -s --format=%H)"
+if diff /etc/setup/installed.db stat/installed.db && [ "$(cat stat/ss-commit)" == "$ss_commit" ] && [ "$(cat stat/obfs-commit)" == "$obfs_commit" ]; then
 	exit 0
 fi
 cp /etc/setup/installed.db stat/
-echo "$git_commit" > stat/commit
+echo "$ss_commit" > stat/ss-commit
+echo "$obfs_commit" > stat/obfs-commit
 
-cd shadowsocks-libev
+function build {
+	pushd $1
+	git submodule update --init
+	./autogen.sh
+	./configure --disable-documentation
+	make
+	make DESTDIR="$HOME/dst" install
+	popd
+}
 
-git submodule update --init
-./autogen.sh
-./configure --disable-documentation
-make
-mkdir ../dst
-make DESTDIR="$HOME/dst" install
+mkdir dst
+build shadowsocks-libev
+build simple-obfs
+cp /bin/sh.exe dst
 
-cd ../shadowsocks-libev-win
-
+cd shadowsocks-libev-win
 rm -f *.exe *.dll
 cp ../dst/usr/local/bin/* .
 cp $(ldd *.exe | awk '$3 ~ /\/usr\/bin\// { print $3 }' | sort | uniq) .
