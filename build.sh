a6085e2c41e394a58e926e8b5a8a6ed1c0b1ae64
DDoSolitary <DDoSolitary@gmail.com>
2018-02-15 11:38:52 +0800

    Remove requirement of sh.exe.

--- build.sh
+++ build.sh
@@ -45,13 +45,53 @@ cp /etc/setup/installed.db stat/
 echo "$ss_commit" > stat/ss-commit
 echo "$obfs_commit" > stat/obfs-commit
 
+pushd shadowsocks-libev
+git apply << EOF
+diff --git a/src/plugin.c b/src/plugin.c
+--- a/src/plugin.c
++++ b/src/plugin.c
+@@ -87,14 +87,6 @@ start_ss_plugin(const char *plugin,
+                 const char *local_port,
+                 enum plugin_mode mode)
+ {
+-    char *cmd = NULL;
+-
+-    size_t plugin_len = strlen(plugin);
+-    size_t cmd_len    = plugin_len + CMD_RESRV_LEN;
+-    cmd = ss_malloc(cmd_len);
+-
+-    snprintf(cmd, cmd_len, "exec %s", plugin);
+-
+     cork_env_add(env, "SS_REMOTE_HOST", remote_host);
+     cork_env_add(env, "SS_REMOTE_PORT", remote_port);
+
+@@ -104,7 +96,7 @@ start_ss_plugin(const char *plugin,
+     if (plugin_opts != NULL)
+         cork_env_add(env, "SS_PLUGIN_OPTIONS", plugin_opts);
+
+-    exec = cork_exec_new_with_params("sh", "-c", cmd, NULL);
++    exec = cork_exec_new(plugin);
+
+     cork_exec_set_env(exec, env);
+
+@@ -112,8 +104,6 @@ start_ss_plugin(const char *plugin,
+
+     int err = cork_subprocess_start(sub);
+
+-    ss_free(cmd);
+-
+     return err;
+ }
+EOF
+popd
+
 mkdir dst
 build shadowsocks-libev
 build simple-obfs
 
 cd shadowsocks-libev-win
 rm -f *.exe *.dll
-cp ../dst/usr/local/bin/* /bin/sh.exe .
+cp ../dst/usr/local/bin/* .
 cp $(ldd *.exe | awk '$3 ~ /\/usr\/bin\// { print $3 }' | sort | uniq) .
 git add -A
 
