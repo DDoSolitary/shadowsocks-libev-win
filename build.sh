63ac361a10ad543672522b37cf39e0414a701d65
DDoSolitary <DDoSolitary@gmail.com>
2019-09-19 01:36:37 +0800

    Fix deleting files.

--- build.sh
+++ build.sh
@@ -32,8 +32,8 @@ cp $(ldd *.exe | awk '$3 ~ /\/usr\/bin\// { print $3 }' | sort | uniq) .
 tar czf binaries.tar.gz *
 
 curl="curl -sSL -u ddosolitary:$BINTRAY_KEY"
-api_prefix=https://api.bintray.com/content/ddosolitary/dev-releases/default/default
+api_prefix=https://api.bintray.com/content/ddosolitary/dev-releases
 file_name=shadowsocks-libev-win-$ARCH.tar.gz
 $curl -X DELETE $api_prefix/$file_name
-$curl -f -T binaries.tar.gz $api_prefix/$file_name
-$curl -f -X POST $api_prefix/publish
+$curl -f -T binaries.tar.gz $api_prefix/default/default/$file_name
+$curl -f -X POST $api_prefix/default/default/publish
