c753994b92bb5e3f76f77f879c3d646585de5870
DDoSolitary <DDoSolitary@gmail.com>
2020-03-12 11:43:27 +0800

    Patch simple-obfs to make Cygwin GCC happy.

--- build.sh
+++ build.sh
@@ -23,6 +23,24 @@ function get-commit {
 git clone https://github.com/shadowsocks/shadowsocks-libev
 git clone https://github.com/shadowsocks/simple-obfs
 
+pushd simple-obfs
+patch -Np1 << EOF
+diff --git a/src/utils.c b/src/utils.c
+index 67cc250..514a001 100644
+--- a/src/utils.c
++++ b/src/utils.c
+@@ -92,7 +92,7 @@ int
+ ss_isnumeric(const char *s) {
+     if (!s || !*s)
+         return 0;
+-    while (isdigit(*s))
++    while (isdigit((int)*s))
+         ++s;
+     return *s == '\0';
+ }
+EOF
+popd
+
 mkdir dst
 build shadowsocks-libev
 build simple-obfs
