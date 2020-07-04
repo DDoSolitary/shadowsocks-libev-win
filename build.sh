d0d07811c08b4f8fd3ae5368daaf35008741a838
DDoSolitary <DDoSolitary@gmail.com>
2020-07-04 20:10:39 +0800

    Add the library files back.

--- build.sh
+++ build.sh
@@ -50,9 +50,6 @@ if [[ "$TOOLCHAIN" == 'mingw' ]]; then
 	echo "$script" | ar -M
 fi
 
-# Removing libraries due to a commercial contract.
-rm *.h *.a *shadowsocks-libev.dll
-
 tar czf binaries.tar.gz *
 
 curl="curl -sSL -u ddosolitary:$BINTRAY_KEY"
